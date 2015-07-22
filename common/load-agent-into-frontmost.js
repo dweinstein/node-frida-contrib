71dc2f5ce7a5c78a90bc92929c32dacc2f1e6f01
David Weinstein <dweinst@insitusec.com>
2015-07-22 10:08:21 -0400

    refactoring

--- common/load-agent-into-frontmost.js
+++ common/load-agent-into-frontmost.js
@@ -4,20 +4,32 @@
 const co = require('co');
 const frida = require('frida');
 const debug = require('debug')('load-agent-into-frontmost');
+const assert = require('assert');
 
-module.exports = co.wrap(function* (agent, onMessage) {
-  const device = yield frida.getUsbDevice();
+module.exports = co.wrap(function* (device, agent, onMessage) {
+  assert(device, 'require device');
+  assert(agent.script, 'require agent.script');
+  assert(agent.name, 'require agent.name');
+
   const application = yield device.getFrontmostApplication();
   if (application === null) {
     throw new Error('no frontmost');
   }
   debug(application);
   const session = yield device.attach(application.pid);
-  const script = yield session.createScript(agent, { name: 'uispy' });
+  const script = yield session.createScript(agent.script, { name: agent.name });
   debug('before load');
   yield script.load();
   debug('after load');
+
+  if (onMessage) {
+    script.events.listen('message', onMessage);
+  } else {
+    script.events.listen('message', function (msg) {
+      console.log(msg);
+    });
+  }
+
   yield script.postMessage({});
-  script.events.listen('message', onMessage);
   debug('loaded');
 });

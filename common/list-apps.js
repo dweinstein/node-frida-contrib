dc1f7335a411faaa992e9cf99fb78f9b8ce058a9
David Weinstein <dweinst@insitusec.com>
2015-09-06 14:50:20 -0400

    add running column to list apps

--- common/list-apps.js
+++ common/list-apps.js
@@ -6,6 +6,7 @@ module.exports = co.wrap(function*(device) {
   const apps = yield device.enumerateApplications();
   const t = new Table();
   apps.forEach((app) => {
+    t.cell('Running', app.pid ? 'R' : '-');
     t.cell('Name', app.name);
     t.cell('Identifier', app.identifier);
     t.cell('PID', app.pid || '-');

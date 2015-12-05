35490e02ec5d04bc22e6d704106786725b534e12
David Weinstein <dweinst@insitusec.com>
2015-12-04 19:15:08 -0500

    default to config (rc) for resolving device

--- common/resolve-device.js
+++ common/resolve-device.js
@@ -6,6 +6,7 @@ const co = require('co');
  * Resolve device whether remote or USB.
 */
 module.exports = co.wrap(function* (opts) {
+  opts = opts || require('../config');
   const device = opts && opts.remote ?
     yield frida.getRemoteDevice() :
     yield frida.getUsbDevice();
